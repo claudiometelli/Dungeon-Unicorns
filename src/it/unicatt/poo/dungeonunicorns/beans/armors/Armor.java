762c6842fb5c4dad8a55eb7e50a0022e8ed6d1a4
elisamangiavacca <85748143+elisamangiavacca@users.noreply.github.com>
2021-07-02 15:16:29 +0200

    Update Armor.java

--- src/it/unicatt/poo/dungeonunicorns/beans/armors/Armor.java
+++ src/it/unicatt/poo/dungeonunicorns/beans/armors/Armor.java
@@ -1,13 +1,27 @@
 package it.unicatt.poo.dungeonunicorns.beans.armors;
 
+/**
+ * A class which represent an armor for the USER? in the game
+ * 
+ * 
+ * @author claudiometelli
+ * @version 1.0.0
+ *
+ */
 public abstract class Armor {
-	
+/* DA RICONTROLLARE I COMMENTI*/	
+	/**
+	 * The strength of the armor
+	 */
 	private int armorLife;
 	
 	public Armor(int armorLife) {
 		this.armorLife = armorLife;
 	}
 	
+	/**
+	 * @return the strength of the armor
+	 */
 	public int getArmorLife() {
 		return armorLife;
 	}
